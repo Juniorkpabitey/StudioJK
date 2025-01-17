d6149aa07fb93930ecabd09d8c6a23866d6b1ef7
Juniorkpabitey <juniorkpabitey123@gmail.com>
2025-01-17 00:25:03 +0000

    moved client files to frontend folder

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='mb-20'>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>Welcome to StudioJK</h1>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est omnis harum impedit. Magnam, 
+          sed assumenda corporis dolor maiores consequatur soluta? ipsum dolor sit amet consectetur, adipisicing elit. 
+          Voluptatum vero ex ea eveniet debitis cum natus non amet laudantium! 
+          Doloribus consectetur rerum laborum voluptatum ratione! Doloribus cumque voluptas qui laborum.
+          </p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Temporibus quibusdam non mollitia eveniet blanditiis et cum quia, adipisci molestias? Non, accusantium laboriosam. 
+          Velit fugiat excepturi impedit repudiandae? Quaerat eum quia, enim, accusamus, facere eos tempora fugit hic tempore 
+          distinctio non aliquam quam modi blanditiis pariatur possimus architecto nostrum eaque nihil. 
+          psum dolor sit amet consectetur adipisicing elit. 
+          Possimus dicta voluptatibus officiis impedit cumque ab nesciunt dignissimos saepe esse. 
+          Nemo repellat necessitatibus quam voluptatem architecto, quas velit recusandae dicta voluptate.
+          </p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Eligendi, veritatis nemo error maiores illo incidunt? Reiciendis cum tenetur, sit suscipit in 
+          facilis laborum amet consectetur numquam, quaerat iste perferendis non sapiente aliquid. Eos odio enim 
+          quam voluptatum neque non hic, corrupti eius sint magni sunt quae beatae perferendis. Maxime vero, error 
+          vel quod, voluptatibus, quibusdam neque eos itaque deserunt obcaecati atque quidem fugiat. Nam cumque sed 
+          fuga quo. Qui delectus blanditiis culpa. Voluptates, architecto eaque delectus deleniti quis fugit animi ullam 
+          ratione repellendus, molestiae magnam natus suscipit 
+          recusandae molestias, vel aliquid corrupti sed! Ea tempora ab incidunt nemo amet animi.
+          </p>
+    </div>
+  )
+}
+
+export default Home
